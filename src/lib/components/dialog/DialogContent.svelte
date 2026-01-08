aecb2fabb52621ef13d0639662a0d4cec5c3717f
samuba <samuel.z.bach@gmail.com>
2026-01-08 11:33:25 +0700

    Prevent default focus behavior on dialog content opening

--- src/lib/components/dialog/DialogContent.svelte
+++ src/lib/components/dialog/DialogContent.svelte
@@ -7,7 +7,7 @@
 	}: Dialog.ContentProps = $props();
 </script>
 
-<Dialog.Content bind:ref {...restProps} onOpenAutoFocus={(e) => e.preventDefault()} class={[
+<Dialog.Content onOpenAutoFocus={(e) => e.preventDefault()} bind:ref {...restProps} class={[
 	'fixed left-1/2 top-1/2 z-50 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-xl border border-bg-elevated bg-bg-secondary p-6 shadow-2xl focus:outline-none',
 	'will-change-transform data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
 	restProps.class
