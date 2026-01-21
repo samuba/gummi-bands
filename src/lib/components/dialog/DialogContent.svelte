533de99bb1d1cc9167a64e3f8c220f22a4e2034a
samuba <samuel.z.bach@gmail.com>
2026-01-21 11:29:46 +0700

    wip

--- src/lib/components/dialog/DialogContent.svelte
+++ src/lib/components/dialog/DialogContent.svelte
@@ -31,9 +31,9 @@
 	bind:ref
 	{...restProps}
 	class={[
-		'fixed left-1/2 top-1/2 z-50 w-full max-w-md -translate-x-1/2 -translate-y-1/2 border border-bg-elevated bg-bg-secondary p-6 shadow-2xl focus:outline-none',
+		'fixed left-1/2 top-1/2 z-50 w-full max-w-md -translate-x-1/2 -translate-y-1/2 bg-bg-secondary p-6 shadow-2xl focus:outline-none',
 		'max-h-dvh overflow-y-auto',
-		!isFullHeight && 'rounded-xl',
+		!isFullHeight && 'rounded-xl border border-bg-elevated',
 		'will-change-transform data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
 		restProps.class
 	]}>
