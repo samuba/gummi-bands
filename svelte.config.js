e03e66efb3944bdc8a5ba7046ee7af2a26d9084a
samuba <samuel.z.bach@gmail.com>
2026-01-21 16:59:09 +0700

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter(),
 		version: {
-			// Poll for new versions every 60 seconds
 			pollInterval: 10000
 		}
 	}
